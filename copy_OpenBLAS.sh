b1aa772b03344364ab6e14e5b604f179a58179e9
Chip Kerchner <ckerchner@tenstorrent.com>
2025-08-12 18:27:52 +0000

    Fix minor changes.

--- copy_OpenBLAS.sh
+++ copy_OpenBLAS.sh
@@ -1,11 +1,10 @@
-export OPENBLAS_HOME="${HOME}/OpenBLAS/kernel/riscv64"
-cp sbgemv_common.c         ${OPENBLAS_HOME}
-cp sbgemv_common_power10.c ${OPENBLAS_HOME}
-cp sbgemv_n.c              ${OPENBLAS_HOME}
-cp sbgemv_n_vsx.c          ${OPENBLAS_HOME}
-cp sbgemv_n_power10.c      ${OPENBLAS_HOME}
-cp sbgemv_t.c              ${OPENBLAS_HOME}
-cp sbgemv_t_vsx.c          ${OPENBLAS_HOME}
-cp sbgemv_t_power10.c      ${OPENBLAS_HOME}
-cp gemm_common.c           ${OPENBLAS_HOME}
-
+export HOME2="/proj_sw/user_dev/ckerchner"
+export OPENBLAS_HOME="${HOME2}/OpenBLAS/kernel/riscv64"
+cp gemm_ncopy_4_rvv.c      ${OPENBLAS_HOME}
+cp gemm_ncopy_8_rvv.c      ${OPENBLAS_HOME}
+cp gemm_ncopy_16_rvv.c     ${OPENBLAS_HOME}
+cp gemm_tcopy_4_rvv.c      ${OPENBLAS_HOME}
+cp gemm_tcopy_8_rvv.c      ${OPENBLAS_HOME}
+cp gemm_tcopy_16_rvv.c     ${OPENBLAS_HOME}
+cp KERNEL.RISCV64_ZVL128B  ${OPENBLAS_HOME}
+cp KERNEL.RISCV64_ZVL256B  ${OPENBLAS_HOME}
