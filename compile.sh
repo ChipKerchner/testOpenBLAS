3757af0d8c269d9fb0eab61fd6635f94762009d9
Chip Kerchner <ckerchner@tenstorrent.com>
2025-09-12 13:43:54 +0000

    Removal of manual unrolling in GEMV - 4X+ speedup (due to lack of register spillage).

--- compile.sh
+++ compile.sh
@@ -2,7 +2,7 @@ export OPT_FLAGS="-O3"
 #export OPT_FLAGS="-O0 -g"
 export COMMON_FLAGS="${OPT_FLAGS} -march=rv64imafdcv_zvl256b_zvfh_zfh -Wall"
 #export COMMON_FLAGS="${OPT_FLAGS} -march=rv64imafdcv_zvl128b_zvfh_zfh -Wall"
-#export COMMON_GFLAGS="${COMMON_FLAGS}"
+export COMMON_GFLAGS="${COMMON_FLAGS}"
 # Linux
 #riscv64-unknown-linux-gnu-g++ ${COMMON_GFLAGS} testOpenBLAS.cpp -o testOpenBLAS
 riscv64-unknown-linux-gnu-clang++ ${COMMON_FLAGS} -Wno-vla-cxx-extension testOpenBLAS.cpp -o testOpenBLAS
