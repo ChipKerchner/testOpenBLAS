574f8c8bbe9a6fec648fa9c9aa2af942a4849350
Chip Kerchner <ckerchner@tenstorrent.com>
2026-02-13 22:20:10 +0000

    Update OpenBLAS copy scripts.

--- copy_OpenBLAS.sh
+++ copy_OpenBLAS.sh
@@ -1,5 +1,9 @@
 export HOME2="/proj_sw/user_dev/ckerchner"
 export OPENBLAS_HOME="${HOME2}/OpenBLAS/kernel/riscv64"
+cp shgemm_kernel_8x8_zvl128b.c  ${OPENBLAS_HOME}
+cp shgemm_kernel_16x8_zvl256b.c ${OPENBLAS_HOME}
+cp sbgemm_kernel_8x8_zvl128b.c  ${OPENBLAS_HOME}
+cp sbgemm_kernel_16x8_zvl256b.c ${OPENBLAS_HOME}
 exit 1
 # Vectorized packing
 cp gemm_ncopy_4_rvv.c      ${OPENBLAS_HOME}
@@ -8,6 +12,10 @@ cp gemm_ncopy_16_rvv.c     ${OPENBLAS_HOME}
 cp gemm_tcopy_4_rvv.c      ${OPENBLAS_HOME}
 cp gemm_tcopy_8_rvv.c      ${OPENBLAS_HOME}
 cp gemm_tcopy_16_rvv.c     ${OPENBLAS_HOME}
+cp gemm_ncopy_8fp_rvv.c    ${OPENBLAS_HOME}
+cp gemm_ncopy_16fp_rvv.c   ${OPENBLAS_HOME}
+cp gemm_tcopy_8fp_rvv.c    ${OPENBLAS_HOME}
+cp gemm_tcopy_16fp_rvv.c   ${OPENBLAS_HOME}
 cp KERNEL.RISCV64_ZVL128B  ${OPENBLAS_HOME}
 cp KERNEL.RISCV64_ZVL256B  ${OPENBLAS_HOME}
 # Small kernels
---
+++ update_OpenBLAS.sh
@@ -0,0 +1,14 @@
+export HOME2="/proj_sw/user_dev/ckerchner"
+export OPENBLAS_HOME="${HOME2}/OpenBLAS/kernel/riscv64"
+exit 1
+# Vectorized packing
+cp ${OPENBLAS_HOME}/gemm_ncopy_4_rvv.c    .
+cp ${OPENBLAS_HOME}/gemm_ncopy_8_rvv.c    .
+cp ${OPENBLAS_HOME}/gemm_ncopy_16_rvv.c   .
+cp ${OPENBLAS_HOME}/gemm_tcopy_4_rvv.c    .
+cp ${OPENBLAS_HOME}/gemm_tcopy_8_rvv.c    .
+cp ${OPENBLAS_HOME}/gemm_tcopy_16_rvv.c   .
+cp ${OPENBLAS_HOME}/gemm_ncopy_8fp_rvv.c  .
+cp ${OPENBLAS_HOME}/gemm_ncopy_16fp_rvv.c .
+cp ${OPENBLAS_HOME}/gemm_tcopy_8fp_rvv.c  .
+cp ${OPENBLAS_HOME}/gemm_tcopy_16fp_rvv.c .
